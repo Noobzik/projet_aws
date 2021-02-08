61056d093dcaa4a8cb0465f3fefa0076c9b1c4b7
Ubuntu <ubuntu@ip-172-31-49-193.ec2.internal>
2021-02-08 21:34:33 +0000

    Removed flask sql

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 # CLIENT FRONT END APPLICATION 1 #
 
 from flask import Flask, render_template, url_for, request, redirect
-from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
